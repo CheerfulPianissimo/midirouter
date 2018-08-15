7cee0472e02be7797aa82513f1a5ed62867694d3
Deuce314 <you@example.com>
2018-08-15 23:35:20 +0530

    Use slightly modified version of rimd Automatically select log file to record to.

--- src/main.rs
+++ src/main.rs
@@ -1,130 +1,159 @@
+extern crate cursive;
 extern crate midir;
 extern crate rimd;
-extern crate cursive;
 
-use midir::{MidiOutput, MidiInput};
-use rimd::{SMFWriter,SMFBuilder,TrackEvent,Event,MidiMessage};
-use std::sync::mpsc::{Sender, Receiver};
-use std::sync::mpsc;
-use std::time::{SystemTime,Duration};
-use std::path::Path;
-use std::thread::{spawn};
-use cursive::{Cursive};
+use cursive::traits::{Boxable, Identifiable, Scrollable};
 use cursive::views::*;
-use cursive::traits::{Scrollable,Boxable,Identifiable};
-/*struct MidiRouter{
-    record_receiver:Receiver<Vec<u8>>,
-    record_sender:Sender<Vec<u8>>,
-    out_conn:MidiOutputConnection,
-    in_conn:MidiInputConnection,
-}*/
+use cursive::Cursive;
+use midir::{MidiInput, MidiOutput};
+use rimd::{Event, MidiMessage, SMFBuilder, SMFWriter, TrackEvent};
+use std::fs::read_dir;
+use std::path::Path;
+use std::sync::mpsc;
+use std::sync::mpsc::{Receiver, Sender};
+use std::thread::spawn;
+use std::time::{Duration, SystemTime};
 
 fn main() {
-    setup();    
+    setup();
 }
 
-fn start(out_port:usize,in_port:usize,path:&str){    
+fn start(out_port: usize, in_port: usize, path: &str) {
     let (tx, rx): (Sender<Vec<u8>>, Receiver<Vec<u8>>) = mpsc::channel();
 
     let midi_out = MidiOutput::new("Output For MidiRouter").unwrap();
     let midi_in = MidiInput::new("Input For MidiRouter").unwrap();
 
     let mut out = midi_out.connect(out_port, "Out").unwrap();
-    let input=midi_in.connect(in_port, "In", move |_stamp, message, record_sender| {
-            let mut msg_vec:Vec<u8>=vec![0;message.len()];
-             for i in 0..message.len(){
-                 msg_vec.insert(i,message[i]);
-             }
-            if msg_vec.len()!=2{
-            tx.send(msg_vec).unwrap();
-            }
-            match out.send(message){
-                Result::Ok(O)=>{},
-                Result::Err(E)=>{println!("Error with message {:?}  {:?}",E,message);}
-            }
-        },())
-        .unwrap();    
-    record(rx,path);   
+    let _input = midi_in
+        .connect(
+            in_port,
+            "In",
+            move |_stamp, message, _record_sender| {
+                let mut msg_vec: Vec<u8> = vec![0; message.len()];
+                for i in 0..message.len() {
+                    msg_vec.insert(i, message[i]);
+                }
+                if msg_vec.len() != 2 {
+                    tx.send(msg_vec).unwrap();
+                }
+                match out.send(message) {
+                    Ok(_) => {}
+                    Err(e) => {
+                        println!("Error with message {:?}  {:?}", e, message);
+                    }
+                }
+            },
+            (),
+        )
+        .unwrap();
+
+    record(rx, path);
 }
 
-fn record(rx:Receiver<Vec<u8>>,path:&str){
-    let mut builder=SMFBuilder::new();
+fn record(rx: Receiver<Vec<u8>>, path: &str) {
+    let mut builder = SMFBuilder::new();
     builder.add_track();
-    let division=120; //120 ticks per beat
+    let division = 120; //120 ticks per beat
     let microsecs_per_tick = 5_000_00 / division as u64;
-    let mut events_number=0;
-    //let mut start_time = duration_to_micros(&SystemTime::now().elapsed().unwrap());
-    loop{ 
-        let start_duration = SystemTime::now();      
-        let msg=rx.recv().unwrap();
+    let mut events_number = 0;
+    loop {
+        let start_duration = SystemTime::now();
+        let msg = rx.recv().unwrap();
 
-        let delta_time =duration_to_micros(&start_duration.elapsed().unwrap());        
-        let delta_ticks=(delta_time)/microsecs_per_tick;
-        
-        builder.add_event(0,TrackEvent{
-            vtime:delta_ticks,
-            event:Event::Midi(MidiMessage{data:msg})
-        });
-        events_number=events_number+1;
+        let delta_time = duration_to_micros(&start_duration.elapsed().unwrap());
+        let delta_ticks = (delta_time) / microsecs_per_tick;
 
-        if events_number>=1000{
-            break;
-        }        
+        builder.add_event(
+            0,
+            TrackEvent {
+                vtime: delta_ticks,
+                event: Event::Midi(MidiMessage { data: msg }),
+            },
+        );
+        events_number = events_number + 1;
+
+        if events_number%1000==0 {
+            let mut smf = builder.clone().result();
+            smf.division = division;
+            let writer = SMFWriter::from_smf(smf);
+            writer.write_to_file(&Path::new(&path)).unwrap();
+        }
     }
-    let mut smf=builder.result();
-    smf.division=division;
-    let writer=SMFWriter::from_smf(smf);    
-    writer.write_to_file(&Path::new(path));
-
 }
 
-fn duration_to_micros(duration:&Duration)->u64{
-    (duration.as_secs() * 1_000_000) +
-                    duration.subsec_micros() as u64
+fn duration_to_micros(duration: &Duration) -> u64 {
+    (duration.as_secs() * 1_000_000) + duration.subsec_micros() as u64
 }
 
-
-fn setup(){
-    let mut siv=Cursive::default();
+fn setup() {
+    let mut siv = Cursive::default();
 
     let midi_out = MidiOutput::new("Output For MidiRouter").unwrap();
     let midi_in = MidiInput::new("Input For MidiRouter").unwrap();
 
-    let mut out_box=LinearLayout::vertical();
-    let mut out_group:RadioGroup<usize>=RadioGroup::new();
+    let mut out_box = LinearLayout::vertical();
+    let mut out_group: RadioGroup<usize> = RadioGroup::new();
     for x in 0..midi_out.port_count() {
-        let button=out_group.button(x, midi_out.port_name(x).unwrap());
+        let button = out_group.button(x, midi_out.port_name(x).unwrap());
         out_box.add_child(button);
-    }     
+    }
 
-    let mut in_box=LinearLayout::vertical();
-    let mut in_group:RadioGroup<usize>=RadioGroup::new();
+    let mut in_box = LinearLayout::vertical();
+    let mut in_group: RadioGroup<usize> = RadioGroup::new();
     for x in 0..midi_in.port_count() {
-        let button=in_group.button(x, midi_in.port_name(x).unwrap());
+        let button = in_group.button(x, midi_in.port_name(x).unwrap());
         in_box.add_child(button);
-    }     
-    let mut hbox=LinearLayout::horizontal();
-    hbox.add_child(Dialog::around(in_box.scrollable().fixed_size((30,10))).title("MIDI Input"));
-    hbox.add_child(Dialog::around(out_box.scrollable().fixed_size((30,10))).title("MIDI Output"));
-   
-   let file_text=EditView::new().content("./test.mid").with_id("path").fixed_width(40);
-   let filebox=LinearLayout::horizontal().child(file_text);
+    }
+    let mut hbox = LinearLayout::horizontal();
+    hbox.add_child(Dialog::around(in_box.scrollable().fixed_size((30, 10))).title("MIDI Input"));
+    hbox.add_child(Dialog::around(out_box.scrollable().fixed_size((30, 10))).title("MIDI Output"));
 
-    let mut vbox=LinearLayout::vertical().child(hbox);
-    vbox.add_child(Dialog::around(filebox).title("Select A File To Record To"));
-    vbox.add_child(Button::new("Start", move |s|{
-        let out_port=out_group.selected_id();
-        let in_port=in_group.selected_id();
-        let pathref = s.call_on_id("path", |view: &mut EditView| {                   
-                    view.get_content().clone()
-            }).unwrap();
-        println!("{}",pathref.as_str());
-        //let path=String::from(file_text.get_content());
-        spawn(move ||{start(out_port,in_port,"./test.mid")});
+    let file_text = TextArea::new()
+        .disabled()
+        .with_id("path")
+        .fixed_size((60,1)).scrollable();
+    let filebox = LinearLayout::horizontal().child(file_text);
+
+    let mut vbox = LinearLayout::vertical().child(hbox);
+    vbox.add_child(Dialog::around(filebox).title("Log"));
+    vbox.add_child(Button::new("Start", move |s| {
+        let mut max = 0;
+        for i in read_dir(Path::new("./")).unwrap() {
+            let path = i.unwrap().path();
+            let extension = match path.extension() {
+                Some(e) => e,
+                None => {
+                    continue;
+                }
+            };
+            if extension.eq("mid") {
+                let file_stem = path.file_stem().unwrap().to_str().unwrap();
+                match file_stem.parse::<u32>() {
+                    Ok(number) => {
+                        if number > max {
+                            max = number;
+                        }
+                    }
+                    Err(_) => continue,
+                };
+            }
+        }
+        let path = format!("{}.mid", max + 1);
+        let out_port = out_group.selected_id();
+        let in_port = in_group.selected_id();
+        s.call_on_id("path", |view: &mut TextArea| {
+            view.set_content(format!(
+                "Joining {} with {}, recording to {}",
+                midi_in.port_name(in_port).unwrap(),
+                midi_out.port_name(out_port).unwrap(),
+                path
+            ));
+        }).unwrap();
+        spawn(move || start(out_port, in_port,&path));
     }));
     siv.add_layer(Dialog::around(vbox));
     siv.add_global_callback('q', |s| s.quit());
 
     siv.run();
-    
 }
